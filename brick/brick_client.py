89330695e0741d97ef81a6be7992708225a16c14
Ben Hoyle <benjhoyle@gmail.com>
2018-02-10 12:21:47 +0000

    Working version

--- brick/brick_client.py
+++ brick/brick_client.py
@@ -21,7 +21,7 @@ right_m = ev3.LargeMotor('outB')
 #m.run_timed(time_sp=3000, speed_sp=500)
 	
 p = ev3.PowerSupply()
-sensors = ev3.list_sensors()
+#sensors = ev3.list_sensors()
  
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
@@ -29,7 +29,7 @@ while 1:
 	volts = p.measured_volts
 	amps = p.measured_amps
 	sensor_state = list()
-	for sensor in sensors:
+	for sensor in ev3.list_sensors():
 		sensor_state.append(sensor.value())
 	robot_state = {
 		'volts':volts, 
